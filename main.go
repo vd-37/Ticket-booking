7dfd2bcb4263e07ae73af70a6474a02469825d87
Vinayaka-Doozie <vinayakadayanand007@gmail.com>
2024-07-02 21:36:07 +0530

    Added some logic, learnt diff bw printf and println and := assignment

--- main.go
+++ main.go
@@ -3,11 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	const conferenceName = "Go Conference"
-	const totalTickets = 50
-	var remainingTickets = 50.6436436436436343
+	conferenceName := "Go Conference"
+	totalTickets := 50
+	var userName, email string
+	var remainingTickets uint = 5
+	var userTickets uint
 	fmt.Printf("Hello, welcome to %v booking application \n", conferenceName)
 	fmt.Printf("We have a total of %v tickets, and remaining of %v tickets \n", totalTickets, remainingTickets)
-	fmt.Printf("Type of conference %T and %T\n", totalTickets, remainingTickets)
+
+	fmt.Printf("Please enter your name: ")
+	fmt.Scan(&userName)
+
+	fmt.Printf("Please enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Printf("Please enter the no of tickets: ")
+	fmt.Scan(&userTickets)
+
+	if userTickets > remainingTickets {
+		fmt.Println("Not enough tickets")
+	} else {
+		remainingTickets -= userTickets
+		fmt.Println("Ticket booking confirmed")
+		fmt.Printf("User %v has booked %v tickets. Email confirmation sent to %v \n", userName, userTickets, email)
+	}
+
+	fmt.Printf("%v tickets are remaining \n", remainingTickets)
 
 }
