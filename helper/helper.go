eb12cbfbc18b1eac94dc854591201a8a4d4add66
VinayakaD <vinayakadayanand007@gmail.com>
2024-12-26 21:03:00 +0530

    helper comments added

--- helper/helper.go
+++ helper/helper.go
@@ -5,6 +5,7 @@ import (
 )
 
 /* Method to validate the user input */
+/* capitalize the first letter of the method(in Go) to make it exportable */
 func ValidateUserEmail(email string) bool {
 	return strings.Contains(email, "@")
 }
